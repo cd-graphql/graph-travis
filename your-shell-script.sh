e68f48c6841683868d1a4ed8fe613471c97893d8
chongduoy <chongduoy@tujia.com>
2019-02-25 22:58:10 +0800

    upd ： 处理远程shell 找不到环境变量的问题

--- your-shell-script.sh
+++ your-shell-script.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
 cd /root/travis
 # 暂时先这种方式进行
-pm2 stop graph-travis
-pm2 delete graph-travis
-rm -rf ./graph-travis
+sudo pm2 stop graph-travis
+sudo pm2 delete graph-travis
+sudo rm -rf ./graph-travis
 git clone https://github.com/cd-graphql/graph-travis.git
 cd ~/travis/graph-travis
-npm i
+yarn i
 pm2 start src/index.js --name="graph-travis"
