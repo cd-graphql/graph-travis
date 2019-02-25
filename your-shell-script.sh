f0bd8a186917a4bc3a0d6533e716863261770169
chongduoy <chongduoy@tujia.com>
2019-02-25 14:54:56 +0800

    upd  :修复部署问题 9

--- your-shell-script.sh
+++ your-shell-script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd /root/travis
-git pull origin master
+# 暂时先这种方式进行
+pm2 stop graph-travis
+pm2 delete graph-travis
+rm -rf ./graph-travis
+git clone https://github.com/cd-graphql/graph-travis.git
+cd ~/travis/graph-travis
 npm i 
-yarn run start:ci
+pm2 start src/index.js --name="graph-travis"
