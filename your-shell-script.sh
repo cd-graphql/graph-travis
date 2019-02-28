4e5386ec4a3ea3003bac86b6fea0bae67a82648a
chongduoy <chongduoy@tujia.com>
2019-02-28 16:24:11 +0800

    udp : 测试engine 是否好用2

--- your-shell-script.sh
+++ your-shell-script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash --login
 cd /root/travis
 source /root/.nvm/nvm.sh
-sudo rm -rf ./graph-travis
+rm -rf ./graph-travis
 git clone https://github.com/cd-graphql/graph-travis.git
 cd ~/travis/graph-travis
 node -v
-npm i
+yarn i
 pm2 restart graph-travis
